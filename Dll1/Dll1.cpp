6fc44941c6bd8f409a632316d0217fcf65f90aa1
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-10-17 22:25:11 +0300

    added implementation to the library

---
+++ Dll1/Dll1.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "Dll1.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+
+
+
+DLL1_API std::string encrypt(std::string text, int key)
+{
+    std::string code = text;
+
+    for (int i = 0; i < text.length(); i++)
+        code[i] = ((code[i] + key) - 32) % 95 + 32;
+    return code;
+}
+
+DLL1_API std::string decrypt(std::string text, int key)
+{
+    std::string code = text;
+
+    for (int i = 0; i < text.length(); i++)
+        code[i] = ((code[i] - key) - 32 + 3 * 95) % 95 + 32;
+    return code;
+}
